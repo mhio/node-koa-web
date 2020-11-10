ab40a17f80c3dd3bf7a29b3ebfb466fe28cd0777
mhio <37205532+mhio@users.noreply.github.com>
2020-11-10 14:37:41 +1100

    add int tests

---
+++ test/int/KoaWebHandler.js
@@ -0,0 +1,40 @@
+/* global expect */
+const Koa = require('koa')
+
+const { KoaWebHandler } = require('../../src/KoaWebHandler')
+const { Taxios } = require('@mhio/taxios')
+
+describe('test::int::KoaWebHandler', function(){
+
+  let app = null
+  let request
+  let logger
+
+  beforeEach(async function(){
+    logger = Taxios.logger()
+    app = new Koa()
+    request = await Taxios.app(app, logger)
+  })
+
+  afterEach(async function(){
+    await request.cleanUp()
+    request = null
+    app = null
+    logger = null
+  })
+
+  it('should generate a koa response from setupRoutes() router', async function(){
+    class MyHandler extends KoaWebHandler {
+      
+      static template_getThePath = 'whatever'
+      static getThePath(ctx) {
+         return 'goes in `whatever` for GET /the/path'
+      }
+
+      static responseOveralls(ctx) {
+         return '<span>some html on /overalls</span>'
+      }
+    }
+  })
+
+})
